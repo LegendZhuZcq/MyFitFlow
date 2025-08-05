ae4abb4423ff7200d9b780d24e7fdcc490edfab2
Chuanqi Zhu <zhu.legend@gmail.com>
2025-08-05 15:13:38 -0700

    firebase initialize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,23 @@ import CalendarView from '@/components/calendar-view';
 import RoutineDesigner from '@/components/routine-designer';
 import { MoveWorkoutDialog } from '@/components/move-workout-dialog';
 import { getInitialWorkouts } from '@/data/sample-workouts';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-t1OHE4M8zZ8A6dPQrXHEpY-yed99b_c",
+  authDomain: "myfitflow.firebaseapp.com",
+  projectId: "myfitflow",
+  storageBucket: "myfitflow.firebasestorage.app",
+  messagingSenderId: "455885025808",
+  appId: "1:455885025808:web:69ec33b2d15410aca6d095"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 const clientSideHydrate = (initialWorkouts: Record<string, Workout>) => {
